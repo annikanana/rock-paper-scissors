c4bf8f5186f45d5a4b2b2a3c9c83238e47154431
Anneke <anneke@live.com.au>
2022-08-07 16:39:29 +1000

    Add functionality for playing 5 rounds

--- script.js
+++ script.js
@@ -14,45 +14,65 @@ function getPlayerChoice() {
 
 function playRound(playerSelection,computerSelection) {
 
-    console.log("Player selection: " + playerSelection);
-    console.log("Computer selection: " + computerSelection);
+    // console.log("Player selection: " + playerSelection);
+    // console.log("Computer selection: " + computerSelection);
 
     if (playerSelection === computerSelection) {    // If the computer and the player has selected the same option, return "Tie"
-        return "Tie";
+        return;
     } else if (playerSelection === "rock") {   
         if (computerSelection === "scissors") {   // If the player has selected rock and the computer has selected scissors, return "Player wins"
-            return "You win! Rock beats Scissors.";
+            return "player";
         } else {    // If the player has selected rock and the computer has selected paper, return "Computer wins"
-            return "You lose! Paper beats Rock.";
+            return "computer";
         }
     } else if (playerSelection === "paper") {  
         if (computerSelection === "rock") { // If the player has selected paper and the computer has selected rock, return "Player wins"
-            return "You win! Paper beats Rock.";
+            return "player";
         } else {    // If the player has selected paper and the computer has selected scissors, return "Computer wins"
-            return "You lose! Scissors beats Paper.";
+            return "computer";
         }
     } else if (playerSelection === "scissors") {
         if (computerSelection === "paper") { // If the player has selected scissors and the computer has selected paper, return "Player wins"
-            return "You win! Scissors beats Paper.";
+            return "player";
         } else {    // If the player has selected scissors and the computer has selected rock, return "Computer wins"
-            return "You lose! Rock beats Scissors.";
+            return "computer";
         }
     } else {    // Else if the player has entered an invalid choice display an error message "Choose either rock, paper or scissors!"
         return "Please enter a valid option: rock, paper or scissors."
     }
 }
 
-//console.log(playRound(getPlayerChoice(),getComputerChoice()));
+// console.log(playRound(getPlayerChoice(),getComputerChoice()));
 
 // Play a game of 5 rounds
 
 function game() {
 
+let playerTally = 0;
+let computerTally = 0;
+
     // For each round, call the playRound function to determine a winner
     for (let i = 0; i < 5; i++) {
-        console.log("Round " + i + ": " + playRound(getPlayerChoice(),getComputerChoice()));
+        // console.log("Round " + (i + 1) + ": " + playRound(getPlayerChoice(),getComputerChoice()));
+        let winner = playRound(getPlayerChoice(),getComputerChoice());
 
+        if (winner === "player") {
+            playerTally++;
+            console.log("You win this round!")
+            console.log("Round " + (i + 1) + ": Player: " + playerTally + " Computer: " + computerTally);
+
+        } else if (winner === "computer")
+        {
+            computerTally++;
+            console.log("You lose this round!");
+            console.log("Round " + (i + 1) + ": Player: " + playerTally + " Computer: " + computerTally);
+        } else {
+            console.log("It's a tie!");
+            console.log("Round " + (i + 1) + ": Player: " + playerTally + " Computer: " + computerTally);
+        }
     }
+
+    console.log("Player tally: " + playerTally + " Computer tally: " + computerTally);
 
 }
 
